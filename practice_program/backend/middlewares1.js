0161337fbb8b04cd6e90f0d8837c7e95643fedb9
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2026-03-17 15:44:23 +0530

    recap of middlewares

--- practice_program/backend/middlewares1.js
+++ practice_program/backend/middlewares1.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.get("/add", function (req, res) {
+// middleware - modifying the request or response object 
+
+let requestCount = 0
+
+function middleware(req, res, next) {
+    requestCount = requestCount + 1;
+    req.name = "niranjan"
+    console.log(`total number of requests are ${requestCount}`)
+    next();
+}
+
+function sum(req, res) {
+
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
+    console.log(req.name);
 
     res.json({ answer: a + b })
-})
+}
+
+app.get("/add", middleware, sum);
 
 
 
